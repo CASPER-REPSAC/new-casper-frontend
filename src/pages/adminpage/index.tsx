a38751af85b3abd58c9300ed62a1bfbcfdd3c984
breakpack <198799879a@gmail.com>
2023-07-31 13:57:44 +0900

    testpush

--- src/pages/adminpage/index.tsx
+++ src/pages/adminpage/index.tsx
@@ -2,32 +2,33 @@ import { isDarkState } from '@src/atoms';
 import { useForm } from 'react-hook-form';
 import { AiOutlineLock, AiOutlineUser } from 'react-icons/ai';
 import { useRecoilValue } from 'recoil';
+import { useState } from 'react';
 import {
-  Form,
-  LoginInput,
-  Label,
-  Row,
   Wrapper,
-  Register_link,
-  LoginButton,
-  LogoWrapper,
+  SideBar,
+  SideBarRow,
+  SideTitle,
 } from './adminpage.style';
-import Image from 'next/image';
-
-interface LoginFormProps {
-  id: string;
-  pw: string;
-}
 
 export default function Login() {
   const isDark = useRecoilValue(isDarkState);
-
-  const { register, watch, handleSubmit } = useForm<LoginFormProps>();
-  // const theme = useContext(ThemeContext);
+  const [isUserOpen, setUserOpen] = useState(false);
 
   return (
     <Wrapper>
+      <SideBar>
+          <SideBarRow>
+            <SideTitle>대시보드</SideTitle>
+          </SideBarRow>
+          <SideBarRow>
+              <SideTitle>사용자관리</SideTitle>
+          </SideBarRow>        
+      </SideBar>
       관리자 페이지 입니다
+      {/* 1. 게시판 관리
+        1-1. 게시판별 카테고리 관리
+      2. 게시물, 댓글관리
+      3. 회원권한관리 */}
     </Wrapper>
   );
 }
