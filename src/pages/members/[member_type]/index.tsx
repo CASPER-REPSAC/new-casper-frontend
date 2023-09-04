f6f2d42aa4949cb43a4583a3fb50f268eef7928e
jijiseong <jiseong5036@gmail.com>
2023-09-04 13:57:23 +0900

    [refactor] 멤버 페이지 리팩토링

--- src/pages/members/[member_type]/index.tsx
+++ src/pages/members/[member_type]/index.tsx
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
 import SideBar from '@src/components/common/SideMenu';
 import PageTitle from '@src/components/common/PageTitle';
 import CommonCenterWrapper from '@src/components/common/Layout/CommonCenterWrapper';
 import { PAGE_TITLE } from '@src/utils/constants';
 import PATH from '@src/utils/urls';
+import MembersSection from '@src/components/pages/members/MembersSection';
 
 function Members() {
   return (
@@ -11,45 +11,10 @@ function Members() {
       <PageTitle pageTitle={PAGE_TITLE.members} />
       <CommonCenterWrapper>
         <SideBar menus={PATH.members} />
-        <Main>
-          <Cards>
-            <MemberCard />
-            <MemberCard />
-            <MemberCard />
-            <MemberCard />
-            <MemberCard />
-            <MemberCard />
-            <MemberCard />
-            <MemberCard />
-          </Cards>
-        </Main>
+        <MembersSection />
       </CommonCenterWrapper>
     </>
   );
 }
-
-const Main = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-const Cards = styled.div`
-  margin-left: 30px;
-  margin-right: 30px;
-  height: 23vw;
-  width: 100%;
-  display: grid;
-  gap: 3px;
-  grid-template-columns: repeat(6, 1fr);
-  box-sizing: border-box;
-`;
-const MemberCard = styled.div`
-  transform: skewX(-18deg);
-  height: 100%;
-  width: 100%;
-
-  background-color: ${({ theme }) => theme.surfaceAlt};
-  opacity: 0.4;
-  margin: 0;
-`;
 
 export default Members;
