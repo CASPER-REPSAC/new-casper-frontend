1d5149ce29e6fe9f7974bc92249ea992067d107e
jijiseong <jiseong5036@gmail.com>
2023-09-15 01:12:15 +0900

    [feat] 게시글 작성(서버 500 error)

--- src/hooks/apis/boards/usePostArticle/index.tsx
+++ src/hooks/apis/boards/usePostArticle/index.tsx
@@ -1,6 +1,8 @@
+import { accessTokenState } from '@src/atoms';
 import { POST_ARTICLE_API } from '@src/utils/apiUrl';
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
+import { useRecoilValue } from 'recoil';
 
 interface PostReqData {
   title: string;
@@ -8,8 +10,14 @@ interface PostReqData {
 }
 
 export default function usePostArticle() {
-  const mutationFn = (params: PostReqData) =>
-    axios.post(POST_ARTICLE_API, params);
+  const accessToken = useRecoilValue(accessTokenState);
+  const mutationFn = (params: PostReqData) => {
+    return axios.post(POST_ARTICLE_API, params, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  };
 
   return useMutation({
     mutationFn,
