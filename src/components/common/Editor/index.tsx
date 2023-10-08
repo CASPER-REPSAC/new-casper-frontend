27dc2b3182792326f6801e02ab7e89017cb585c7
jijiseong <jiseong5036@gmail.com>
2023-10-08 20:23:30 +0900

    [fix] 에디터 제거 (임시)

--- src/components/common/Editor/index.tsx
+++ src/components/common/Editor/index.tsx
@@ -1,23 +1,7 @@
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { styled } from 'styled-components';
-import { useFormContext } from 'react-hook-form';
-import { PostReqData } from '@src/types/PostTypes';
 
 function Editor() {
-  const { setValue } = useFormContext<PostReqData>();
-
-  return (
-    <Wrapper>
-      <CKEditor
-        editor={ClassicEditor}
-        data=""
-        onChange={(event, editor) => {
-          setValue('content', editor.getData());
-        }}
-      />
-    </Wrapper>
-  );
+  return <Wrapper />;
 }
 
 const Wrapper = styled.div`
