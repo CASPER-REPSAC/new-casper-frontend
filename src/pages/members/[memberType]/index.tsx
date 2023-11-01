03913651d8ab59b37e9edd741975294cc5c6baa6
jijiseong <jiseong5036@gmail.com>
2023-11-01 21:40:59 +0900

    fix: mockdata 수정

--- src/pages/members/[memberType]/index.tsx
+++ src/pages/members/[memberType]/index.tsx
@@ -6,7 +6,7 @@ import handleErrorStaticProps from '@src/utils/handleErrorStaticProps';
 import { MemberProfile } from '@src/types/memberTypes';
 import MemberLayout from '@src/components/Layout/MemberLayout';
 import { ReactElement } from 'react';
-import axios from 'axios';
+import data from '@src/utils/mockdata';
 
 interface Props {
   memberList: MemberProfile[];
@@ -19,7 +19,7 @@ function Members({ memberList }: Props) {
 Members.getLayout = (page: ReactElement) => <MemberLayout>{page}</MemberLayout>;
 
 export const getStaticPaths: GetStaticPaths = () => {
-  const memberTypes = ['active', 'graduate'];
+  const memberTypes = ['active', 'graduate', 'rest'];
 
   const paths = memberTypes.map((memberType) => {
     return { params: { memberType } };
@@ -33,9 +33,7 @@ export const getStaticPaths: GetStaticPaths = () => {
 export const getStaticProps: GetStaticProps = handleErrorStaticProps(
   async () => {
     // const { data } = await axios.get(`${API_URL}${ALL_MEMEBER_API}`);
-    const { data } = await axios.get<{ memberList: MemberProfile[] }>(
-      'http://localhost:3000/mock/memberList.json',
-    );
+
     return {
       props: {
         memberList: data.memberList,
