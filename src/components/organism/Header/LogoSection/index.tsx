037f5e7ab8052cfc84713aa4840dd2e443286833
jijiseong <jiseong5036@gmail.com>
2023-11-18 20:31:04 +0900

    refactor: logoSection 코드 간소화

--- src/components/organism/Header/LogoSection/index.tsx
+++ src/components/organism/Header/LogoSection/index.tsx
@@ -1,23 +1,21 @@
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { memo } from 'react';
+import { styled } from 'styled-components';
+import { useRecoilValue } from 'recoil';
 import { isDarkState } from '@src/atoms';
 import { PATH } from '@src/constants/urls';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import { memo } from 'react';
-import { useRecoilValue } from 'recoil';
-import { styled } from 'styled-components';
 
 function LogoSection() {
-  const router = useRouter();
+  const { pathname, push } = useRouter();
   const isDark = useRecoilValue(isDarkState);
 
-  const isHome = router.pathname === PATH.home.url;
-  const isDarkHome = isDark || isHome;
-  const logoSrc = isDarkHome
-    ? '/casper_logo_white.png'
-    : '/casper_logo_black.png';
+  const isHome = pathname === PATH.home.url;
+  const logoSrc =
+    isDark || isHome ? '/casper_logo_white.png' : '/casper_logo_black.png';
 
   return (
-    <Logo onClick={() => router.push(PATH.home.url)}>
+    <Logo onClick={() => push(PATH.home.url)}>
       <LogoImg
         src={logoSrc}
         alt="casper logo"
