e9c05cbb3ce9d910a209722897fd57cd973a6971
jijiseong <jiseong5036@gmail.com>
2023-12-01 19:25:05 +0900

    fix: admin page 에러 수정

--- app/(route)/admin/users/log/[page]/index.tsx
+++ app/(route)/admin/users/log/[page]/index.tsx
@@ -1,6 +1,5 @@
 import UserLogTable from 'app/(route)/admin/users/log/[page]/_components/UserLogTable';
 import { AdminCenterWrapper } from 'app/_components/common';
-import { GetStaticPaths, GetStaticProps } from 'next';
 import styled from 'styled-components';
 
 function UserLogPage() {
@@ -11,25 +10,6 @@ function UserLogPage() {
     </Wrapper>
   );
 }
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const mockData = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-  const paths = mockData.map((value) => {
-    return {
-      params: {
-        page: value,
-      },
-    };
-  });
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  return { props: {} };
-};
 
 const Wrapper = styled(AdminCenterWrapper)`
   display: flex;
