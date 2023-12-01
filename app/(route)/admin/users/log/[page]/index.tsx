94d1a4295adf04256a5557163d11731d4d56f72a
jijiseong <jiseong5036@gmail.com>
2023-12-01 19:31:15 +0900

    fix: server component 에러 수정

--- app/(route)/admin/users/log/[page]/index.tsx
+++ app/(route)/admin/users/log/[page]/index.tsx
@@ -1,20 +1,13 @@
 import UserLogTable from 'app/(route)/admin/users/log/[page]/_components/UserLogTable';
 import { AdminCenterWrapper } from 'app/_components/common';
-import styled from 'styled-components';
 
 function UserLogPage() {
   return (
-    <Wrapper>
+    <AdminCenterWrapper>
       <UserLogTable />
       {/* <BoardFooter maxPage={10} curPage={curPage} /> */}
-    </Wrapper>
+    </AdminCenterWrapper>
   );
 }
-
-const Wrapper = styled(AdminCenterWrapper)`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-`;
 
 export default UserLogPage;
