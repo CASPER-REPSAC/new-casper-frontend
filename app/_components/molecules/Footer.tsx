0a1c8ee280b42dac52598fd8550228576fb24292
jijiseong <jiseong5036@gmail.com>
2023-12-02 21:35:36 +0900

    refactor: tailwind - Footer Popup

--- app/_components/molecules/Footer.tsx
+++ app/_components/molecules/Footer.tsx
@@ -1,5 +1,3 @@
-import styled from 'styled-components';
-
 import {
   CopyrightIcon,
   FacebookIcon,
@@ -11,49 +9,23 @@ import { FOOTER } from 'app/_constants/label';
 
 function Footer() {
   return (
-    <Wrapper>
+    <div className="mt-24 h-12 w-full bg-gray-900">
       <div className="common-center flex h-full items-center justify-between">
-        <Sns>
+        <div className="flex w-28 justify-between">
           <GithubIcon size={ICON_SIZE.medium} />
           <FacebookIcon size={ICON_SIZE.medium} />
           <InstagramIcon size={ICON_SIZE.medium} />
-        </Sns>
-        <Info>
-          <Span>{FOOTER.location}</Span>
-          <Span>
+        </div>
+        <div className="flex flex-col text-right text-xs">
+          <span>{FOOTER.location}</span>
+          <span className="flex items-end justify-end">
             <CopyrightIcon />
             {FOOTER.copyRight}
-          </Span>
-        </Info>
+          </span>
+        </div>
       </div>
-    </Wrapper>
+    </div>
   );
 }
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 50px;
-  background-color: ${(props) => props.theme.surfaceAlt};
-  color: ${(props) => props.theme.textDefault};
-  margin-top: 100px;
-`;
-
-const Sns = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100px;
-`;
-const Info = styled.div`
-  font-size: 1.2rem;
-  font-weight: lighter;
-  display: flex;
-  text-align: right;
-  flex-direction: column;
-`;
-const Span = styled.span`
-  display: flex;
-  align-items: flex-end;
-  justify-content: flex-end;
-`;
 
 export default Footer;
