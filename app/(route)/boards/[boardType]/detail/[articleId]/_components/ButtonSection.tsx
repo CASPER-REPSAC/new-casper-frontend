ebc9f0d14daa85acb8d5952e977582dce1294c65
jijiseong <jiseong5036@gmail.com>
2023-12-04 03:40:47 +0900

    fix: 글 수정 버그 수정

--- app/(route)/boards/[boardType]/detail/[articleId]/_components/ButtonSection.tsx
+++ app/(route)/boards/[boardType]/detail/[articleId]/_components/ButtonSection.tsx
@@ -1,20 +1,21 @@
 import { DefaultButton } from 'app/_components/common';
 import {
-  useArticleDetail,
   useDeleteArticleMutation,
   useUpdateArticleMutation,
 } from 'app/_hooks/apis/boards';
 import { editableState } from 'app/_store/detailPageAtoms';
+import { useRouter } from 'next/navigation';
 import { useFormContext } from 'react-hook-form';
 import { useRecoilState } from 'recoil';
 
 function ButtonSection({ articleId }: { articleId: string }) {
   const methods = useFormContext();
+  const { refresh } = useRouter();
+
   const [editable, setEditable] = useRecoilState(editableState);
   const { mutate: mutateDeletion } = useDeleteArticleMutation(articleId);
   const { mutateAsync: mutateUpdateAsync } =
     useUpdateArticleMutation(articleId);
-  const { refetch } = useArticleDetail(articleId);
 
   const deleteArticle = () => {
     mutateDeletion();
@@ -23,14 +24,15 @@ function ButtonSection({ articleId }: { articleId: string }) {
     setEditable(true);
   };
   const completeModification = async () => {
-    setEditable(false);
     await mutateUpdateAsync({
       articleId,
       title: methods.getValues('title'),
       content: methods.getValues('content'),
     });
-    refetch();
+    setEditable(false);
+    refresh();
   };
+
   return (
     <div className="flex gap-4">
       <DefaultButton
