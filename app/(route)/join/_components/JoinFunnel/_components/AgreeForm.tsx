86c6b133516496dd7e23b0bb8e11f7c09bf4c83f
jijiseong <jiseong5036@gmail.com>
2023-12-15 13:42:48 +0900

    design: 홈 디자인 수정, 회원가입 ui 버그 수정

--- app/(route)/join/_components/JoinFunnel/_components/AgreeForm.tsx
+++ app/(route)/join/_components/JoinFunnel/_components/AgreeForm.tsx
@@ -18,7 +18,7 @@ function AgreeForm({ onNext }: Props) {
         {...agreeRegister}
         label="개인 정보 수집 이용 동의서 (필수)"
       />
-      <div className="flex flex-col gap-4 rounded border border-solid border-gray-300 bg-gray-800 p-4 text-lg font-thin">
+      <div className="flex flex-col gap-4 rounded border border-solid border-slate-300 bg-sky-50 p-4 text-lg font-thin dark:border-slate-600 dark:bg-gray-800">
         <li>
           <strong>수집 목적:</strong> 이용자에게 최적의 서비스를 제공하기 위함
         </li>
--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -1,26 +1,10 @@
-import { getOnePageArticleList } from 'app/_service/article';
-import { SirenIcon } from 'app/_components/icons';
-import { ICON_SIZE } from 'app/_constants/size';
-import ArticlePreview from './_components/ArticlePreview';
-
 async function HomePage() {
-  const notice = await getOnePageArticleList({
-    boardType: 'notice_board',
-    page: '1',
-    category: 'all',
-  });
-
   return (
     <div className="common-center lg:absolute-center flex-center mt-24 w-full flex-col text-center lg:mt-0">
-      <h1 className="text-6xl font-bold">WELCOME !</h1>
+      <h1 className="text-6xl font-bold">WELCOME!</h1>
       <div className="h-full w-full text-6xl font-bold text-sky-300">
         CASPER
       </div>
-      <div className="mt-16 flex flex-col gap-16 lg:flex-row">
-        <ArticlePreview articleList={notice.articleList} />
-        <ArticlePreview articleList={notice.articleList} />
-      </div>
-      <SirenIcon size={ICON_SIZE.medium} className="self-end fill-red-400" />
     </div>
   );
 }
