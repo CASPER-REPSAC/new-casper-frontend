00babc1d2075dfc7b0b6f32becfbed183cea0088
jijiseong <jiseong5036@gmail.com>
2023-12-22 00:02:43 +0900

    feat: join - progress bar 현재 상태 CSS 명확히 표시

--- app/(route)/join/_components/ProgressBar.tsx
+++ app/(route)/join/_components/ProgressBar.tsx
@@ -1,7 +1,10 @@
 import { CheckIcon, DownArrowIcon } from 'app/_components/icons';
 import { ICON_SIZE } from 'app/_constants/size';
+import { PATH } from 'app/_constants/urls';
 import { useFunnel } from 'app/_hooks';
 import { JoinFormData } from 'app/_types/joinTypes';
+import { Url } from 'next/dist/shared/lib/router/router';
+import Link from 'next/link';
 import { useFormContext } from 'react-hook-form';
 
 function ProgressBar() {
@@ -19,26 +22,46 @@ function ProgressBar() {
   return (
     <div className="flex justify-around">
       <Status
+        href={{
+          pathname: PATH.user.join.url,
+          query: { 'funnel-step': 'agree' },
+        }}
         active={funnelStep === 'agree'}
         label="개인 정보 수집 동의"
         isComplete={agreeComplete}
       />
       <Status
+        href={{
+          pathname: PATH.user.join.url,
+          query: { 'funnel-step': 'email' },
+        }}
         active={funnelStep === 'email'}
         label="이메일 인증"
         isComplete={emailComplete}
       />
       <Status
+        href={{
+          pathname: PATH.user.join.url,
+          query: { 'funnel-step': 'name' },
+        }}
         active={funnelStep === 'name'}
         label="닉네임 설정"
         isComplete={nicknameComplete}
       />
       <Status
+        href={{
+          pathname: PATH.user.join.url,
+          query: { 'funnel-step': 'id' },
+        }}
         active={funnelStep === 'id'}
         label="아이디 설정"
         isComplete={idComplete}
       />
       <Status
+        href={{
+          pathname: PATH.user.join.url,
+          query: { 'funnel-step': 'password' },
+        }}
         active={funnelStep === 'password'}
         label="패스워드 설정"
         isComplete={passwordComplete}
@@ -51,9 +74,10 @@ interface Props {
   isComplete: boolean;
   label: string;
   active?: boolean;
+  href: Url;
 }
 
-function Status({ label, isComplete, active = false }: Props) {
+function Status({ href, label, isComplete, active = false }: Props) {
   return (
     <div className="relative flex w-7 flex-col items-center">
       {active && (
@@ -62,18 +86,24 @@ function Status({ label, isComplete, active = false }: Props) {
           className="absolute -top-6 animate-bounce fill-indigo-600 dark:fill-sky-400"
         />
       )}
-      <CheckIcon
-        size={ICON_SIZE.medium}
-        className={`${
-          isComplete
-            ? 'fill-indigo-400 dark:fill-sky-400'
-            : 'fill-slate-300 dark:fill-slate-600'
-        }`}
-      />
+      <Link href={href}>
+        <CheckIcon
+          size={ICON_SIZE.medium}
+          className={`${
+            isComplete
+              ? 'fill-indigo-600 dark:fill-sky-400'
+              : 'fill-slate-300 dark:fill-slate-600'
+          }`}
+        />
+      </Link>
       <div
-        className={`${
-          active ? 'text-black' : 'text-slate-400'
-        } absolute -bottom-6 w-max text-center text-xs dark:text-slate-600`}
+        className={`
+        ${
+          isComplete
+            ? 'text-indigo-600 dark:text-sky-400'
+            : 'text-slate-300 dark:text-slate-600'
+        }
+        absolute -bottom-6 w-max text-center text-xs dark:text-slate-600`}
       >
         {label}
       </div>
