44ea099e4898d5d2a102bd61b0bd01b91bdce044
jijiseong <jiseong5036@gmail.com>
2023-12-23 17:47:55 +0900

    fix: get cookie 에러 수정

--- app/_hooks/apis/boards/useOnePageArticleList.ts
+++ app/_hooks/apis/boards/useOnePageArticleList.ts
@@ -1,6 +1,8 @@
 import { OnePageOfArticleList } from 'app/_types/boardTypes';
 import { useQuery } from '@tanstack/react-query';
 import { getOnePageArticleList } from 'app/_service/article';
+import { useRecoilValue } from 'recoil';
+import { accessTokenState } from 'app/_store/permissionAtoms';
 
 function useOnePageArticleList(
   {
@@ -10,11 +12,13 @@ function useOnePageArticleList(
   }: { boardType: string; page: string; category?: string },
   initialData?: OnePageOfArticleList,
 ) {
+  const accessToken = useRecoilValue(accessTokenState);
   const queryKey = ['onePageArticleList', boardType, page];
 
   const queryFn = async () => {
     const data = await getOnePageArticleList(
       { boardType, page, category },
+      accessToken,
       true,
     );
     return data;
