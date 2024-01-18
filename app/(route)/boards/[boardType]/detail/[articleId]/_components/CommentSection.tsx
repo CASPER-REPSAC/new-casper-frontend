b6d2499db8537ac8861aa4f9fc66873c872ccd39
jijiseong <jiseong5036@gmail.com>
2024-01-18 21:23:43 +0900

    feat: 본인 댓글만 수정/삭제 버튼 노출

--- app/(route)/boards/[boardType]/detail/[articleId]/_components/CommentSection.tsx
+++ app/(route)/boards/[boardType]/detail/[articleId]/_components/CommentSection.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import { useRecoilValue } from 'recoil';
 import { DefaultButton } from '@app/_components/common';
 import { useComments } from '@app/_hooks/apis/boards';
+import { myProfileState } from '@app/_store/permissionAtoms';
 import Avatar from './common/Avatar';
 
 interface Props {
@@ -18,7 +20,7 @@ function CommentSection({ articleId }: Props) {
       {comments.map(({ nickname, modifiedAt, text, commentId }) => (
         <Comment
           key={commentId}
-          name={nickname}
+          nickname={nickname}
           date={modifiedAt}
           content={text}
         />
@@ -28,27 +30,34 @@ function CommentSection({ articleId }: Props) {
 }
 
 interface CommentProps {
-  name: string;
+  nickname: string;
   date: string;
   content: string;
 }
 
-function Comment({ name, date, content }: CommentProps) {
+function Comment({ nickname, date, content }: CommentProps) {
+  const myProfile = useRecoilValue(myProfileState);
+  const isMyComment = myProfile?.nickname === nickname;
+
   return (
     <div className="flex items-start justify-between gap-8">
       <Avatar className="shrink-0" src="/defaultprofile.webp" />
       <div className="flex flex-1 flex-col">
         <div className="mb-2 flex items-end">
-          <span className="mr-4">{name}</span>
+          <span className="mr-4">{nickname}</span>
           <span className="text-sm font-thin">{date}</span>
         </div>
         <div>{content}</div>
       </div>
       <div className="flex gap-4">
-        <DefaultButton size="sm">수정</DefaultButton>
-        <DefaultButton size="sm" theme="danger">
-          삭제
-        </DefaultButton>
+        {isMyComment && (
+          <>
+            <DefaultButton size="sm">수정</DefaultButton>
+            <DefaultButton size="sm" theme="danger">
+              삭제
+            </DefaultButton>
+          </>
+        )}
       </div>
     </div>
   );
