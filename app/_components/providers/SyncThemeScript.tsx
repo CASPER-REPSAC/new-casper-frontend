79550a4734ce109712c91b403cda6a3aa09807b8
jijiseong <jiseong5036@gmail.com>
2024-04-07 20:26:21 +0900

    fix: build error

--- app/_components/providers/SyncThemeScript.tsx
+++ app/_components/providers/SyncThemeScript.tsx
@@ -16,6 +16,7 @@ function SyncThemeScript() {
       default:
         htmlElement.classList.remove('dark');
     }
+    alert('test');
   };
 
   const scriptStr = `
--- app/_hooks/index.ts
+++ app/_hooks/index.ts
@@ -1,6 +1,5 @@
 'use client';
 
-export { default as useBlockNoteTheme } from './useBlockNoteTheme';
 export { default as useFunnel } from './useFunnel';
 export { default as usePagination } from './usePagination';
 export { default as usePopup } from './usePopup';
