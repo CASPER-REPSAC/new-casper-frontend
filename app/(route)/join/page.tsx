07569aa9fee8f8d9f2ae76c9010288a1967a5c8a
jijiseong <jiseong5036@gmail.com>
2024-04-07 20:32:31 +0900

    fix: build error

--- app/(route)/join/page.tsx
+++ app/(route)/join/page.tsx
@@ -11,11 +11,11 @@ function JoinPage() {
     <div className="small-center absolute-center flex flex-col justify-center gap-12">
       <JoinFormProvider>
         <form className="flex min-h-[400px] flex-col items-center justify-between gap-4">
-          <ProgressBar />
           <Suspense>
+            <ProgressBar />
             <JoinFunnel />
+            <NextButton />
           </Suspense>
-          <NextButton />
         </form>
       </JoinFormProvider>
     </div>
