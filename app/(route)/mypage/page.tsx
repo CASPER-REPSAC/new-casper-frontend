1247930ce63744be8f6615049ab80d65b9009986
jijiseong <jiseong5036@gmail.com>
2024-06-23 23:55:17 +0900

    refactor: 유저 정보 수정

--- app/(route)/mypage/page.tsx
+++ app/(route)/mypage/page.tsx
@@ -1,27 +1,57 @@
 'use client';
 
-import { myProfileState } from '@app/_store/permissionAtoms';
-import { MyProfile } from '@app/_types/userTypes';
-import { FormProvider, useForm } from 'react-hook-form';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import { useRecoilValue } from 'recoil';
-import MyAvatarForm from './_components/MyAvatarForm';
-import MyInfoForm from './_components/MyInfoForm';
-import ButtonSection from './_components/ButtonSection';
+import { myProfileState } from '@app/_store/permissionAtoms';
+import { ProfileUpdateForm } from '@app/_types/userTypes';
+import useProfileUpdateMutation from '@app/_hooks/apis/user/useProfileUpdateMutation';
+import { Button } from '@app/_shadcn/components/ui/button';
+import MyAvatarInput from './_components/MyAvatarInput';
+import MyInfoSection from './_components/MyInfoSection';
 
 function MyPage() {
   const myProfile = useRecoilValue(myProfileState);
-  const methods = useForm<MyProfile>(
-    myProfile ? { defaultValues: myProfile } : undefined,
-  );
+  const methods = useForm<ProfileUpdateForm>({
+    defaultValues: {
+      introduce: myProfile ? myProfile.introduce : '',
+      name: myProfile ? myProfile.name : '',
+      nickname: myProfile ? myProfile.nickname : '',
+      role: myProfile ? myProfile.role : '',
+      homepage: myProfile ? myProfile.homepage : '',
+    },
+  });
+  const { handleSubmit } = methods;
+  const { mutate } = useProfileUpdateMutation();
+
+  const onValid: SubmitHandler<ProfileUpdateForm> = ({
+    profileImgPath,
+    nickname,
+    introduce,
+    homepage,
+    name,
+  }) => {
+    mutate({
+      name,
+      nickname,
+      introduce,
+      homepage,
+      profileImgPath,
+    });
+  };
 
   return (
-    <div className="small-center flex flex-col gap-12">
+    <form
+      onSubmit={handleSubmit(onValid)}
+      className="small-center flex flex-col gap-12"
+    >
       <FormProvider {...methods}>
-        <MyAvatarForm />
-        <MyInfoForm />
-        <ButtonSection />
+        <MyAvatarInput />
+        <MyInfoSection />
+        <Button type="submit" size="lg">
+          저장
+        </Button>
       </FormProvider>
-    </div>
+    </form>
   );
 }
 
--- app/_hooks/apis/user/useProfileUpdateMutation.ts
+++ app/_hooks/apis/user/useProfileUpdateMutation.ts
@@ -1,28 +1,19 @@
-import { PROFILE_UPDATE_API } from '@app/_constants/apiUrl';
-import { POPUP_DURATION } from '@app/_constants/duration';
 import { TOAST_TITLE } from '@app/_constants/message';
+import userService from '@app/_service/userService';
 import { useToast } from '@app/_shadcn/components/ui/use-toast';
-import { bearerTokenState } from '@app/_store/permissionAtoms';
 import { ProfileUpdateRequset } from '@app/_types/userTypes';
 import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
-import { useRecoilValue } from 'recoil';
 
 function useProfileUpdateMutation() {
   const { toast } = useToast();
-  const bearerToken = useRecoilValue(bearerTokenState);
 
   const mutationFn = async (data: ProfileUpdateRequset) =>
-    axios.post(`/proxy${PROFILE_UPDATE_API}`, data, {
-      headers: {
-        Authorization: bearerToken,
-      },
-    });
+    userService.updateProfile(data);
 
   const onSuccess = () => {
     toast({
+      title: TOAST_TITLE.error,
       description: '프로필이 업데이트 되었어요.',
-      duration: POPUP_DURATION.medium,
     });
   };
 
