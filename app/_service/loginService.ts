9af1d87a2ad6f22e62f6cdbec00539a02c142e15
jijiseong <jiseong5036@gmail.com>
2024-07-01 13:22:01 +0900

    fix: github login api url

--- app/_service/loginService.ts
+++ app/_service/loginService.ts
@@ -2,7 +2,7 @@ import Service from './service';
 
 class LoginService extends Service {
   loginGithub({ code, redirectUri }: { code: string; redirectUri: string }) {
-    return this.axiosExtend.post('/user/github', { code, redirectUri });
+    return this.axiosExtend.post('/api/user/github', { code, redirectUri });
   }
 }
 
