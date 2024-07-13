9b9e01a8b4dd7d8054aa50e81da96c8acc63fddb
jijiseong <jiseong5036@gmail.com>
2024-07-13 17:04:47 +0900

    feat: ButtonWithDialogCheck

--- app/(route)/mypage/_components/AccountSettingSection.tsx
+++ app/(route)/mypage/_components/AccountSettingSection.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import ButtonWithDialogCheck from '@app/_components/common/WithDialogCheck';
 import useWithdrawalMutation from '@app/_hooks/apis/user/useWithdrawalMutation';
 import { Button } from '@app/_shadcn/components/ui/button';
 import { myProfileState } from '@app/_store/permissionAtoms';
@@ -21,13 +22,16 @@ function AccountSettingSection() {
       <Button className="w-full" variant="secondary">
         비밀번호 변경(개발 중이에요!)
       </Button>
-      <Button
+      <ButtonWithDialogCheck
+        title="회원 탈퇴"
+        description="정말 탈퇴하시겠어요?"
         className="w-full"
+        confirmVariant="destructive"
         variant="destructive"
-        onClick={() => onWithdrawalClick()}
+        onClick={onWithdrawalClick}
       >
         회원 탈퇴
-      </Button>
+      </ButtonWithDialogCheck>
     </section>
   );
 }
---
+++ app/_components/common/WithDialogCheck.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@app/_shadcn/components/ui/dialog';
+import { Button, ButtonProps } from '@app/_shadcn/components/ui/button';
+
+interface Props extends ButtonProps {
+  title: string;
+  description: string;
+  confirmVariant?: ButtonProps['variant'];
+  cancelVariant?: ButtonProps['variant'];
+}
+
+function ButtonWithDialogCheck({
+  title,
+  description,
+  onClick,
+  confirmVariant,
+  cancelVariant,
+  ...props
+}: Props) {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button {...props} />
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+        </DialogHeader>
+        <div>{description}</div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant={cancelVariant || 'secondary'}>취소</Button>
+          </DialogClose>
+          <DialogClose asChild>
+            <Button variant={confirmVariant || 'default'} onClick={onClick}>
+              확인
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default ButtonWithDialogCheck;
