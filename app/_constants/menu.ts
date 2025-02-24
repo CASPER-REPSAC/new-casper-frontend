a08507ea69f6b6cde6db20eea074cebd9dbf32f9
jijiseong <jiseong5036@gmail.com>
2025-02-24 23:36:52 +0900

    fix: menu access permission

--- app/_constants/menu.ts
+++ app/_constants/menu.ts
@@ -7,21 +7,21 @@ export const MEMBER_TABS = [
     href: PATH.members.active.url,
     name: PATH.members.active.name,
     startWith: PATH.members.active.url,
-    accessibleRoles: ['admin', 'active', 'associate', 'guest'],
+    accessibleRoles: ['admin', 'active', 'graduate', 'associate', 'guest'],
   },
   {
     key: MEMBER_TYPE.rest,
     href: PATH.members.rest.url,
     name: PATH.members.rest.name,
     startWith: PATH.members.rest.url,
-    accessibleRoles: ['admin', 'active', 'associate', 'guest'],
+    accessibleRoles: ['admin', 'active', 'graduate', 'associate', 'guest'],
   },
   {
     key: MEMBER_TYPE.graduate,
     href: PATH.members.graduate.url,
     name: PATH.members.graduate.name,
     startWith: PATH.members.graduate.url,
-    accessibleRoles: ['admin', 'active', 'associate', 'guest'],
+    accessibleRoles: ['admin', 'active', 'graduate', 'associate', 'guest'],
   },
   {
     key: MEMBER_TYPE.all,
@@ -36,24 +36,24 @@ export const INTRA_TABS = [
   {
     name: 'Wiki',
     href: INTRA_PATH.wiki,
-    accessibleRoles: ['admin', 'active', 'associate', 'guest'],
+    accessibleRoles: ['admin', 'active', 'graduate', 'associate', 'guest'],
   },
   {
     name: 'Nas',
     href: INTRA_PATH.nas,
-    accessibleRoles: ['admin', 'active', 'associate', 'guest'],
+    accessibleRoles: ['admin', 'active', 'graduate', 'associate', 'guest'],
   },
   {
     name: 'Recruit',
     href: INTRA_PATH.recruit,
-    accessibleRoles: ['admin', 'active', 'associate', 'guest'],
+    accessibleRoles: ['admin', 'active', 'graduate', 'associate', 'guest'],
   },
 ];
 
 export const BOARD_TABS = [
   {
     name: '공지사항',
-    accessibleRoles: ['admin', 'active', 'associate', 'guest'],
+    accessibleRoles: ['admin', 'active', 'graduate', 'associate', 'guest'],
     key: BOARD_TYPE.notice,
     href: NEW_PATH.boardList.url({
       boardType: BOARD_TYPE.notice,
@@ -69,8 +69,8 @@ export const BOARD_TABS = [
       .split('/list')[0],
   },
   {
-    name: 'active 게시판',
-    accessibleRoles: ['admin', 'active'],
+    name: '정회원 게시판',
+    accessibleRoles: ['admin', 'active', 'graduate', 'associate'],
     key: BOARD_TYPE.full,
     href: NEW_PATH.boardList.url({
       boardType: BOARD_TYPE.full,
@@ -87,7 +87,7 @@ export const BOARD_TABS = [
   },
   {
     name: '졸업생 게시판',
-    accessibleRoles: ['admin', 'active'],
+    accessibleRoles: ['admin', 'active', 'graduate'],
     key: BOARD_TYPE.graduate,
     href: NEW_PATH.boardList.url({
       boardType: BOARD_TYPE.graduate,
@@ -103,8 +103,8 @@ export const BOARD_TABS = [
       .split('/list')[0],
   },
   {
-    name: 'associate 게시판',
-    accessibleRoles: ['admin', 'active', 'associate'],
+    name: '준회원 게시판',
+    accessibleRoles: ['admin', 'active', 'graduate', 'associate'],
     key: BOARD_TYPE.associate,
     href: NEW_PATH.boardList.url({
       boardType: BOARD_TYPE.associate,
@@ -121,7 +121,7 @@ export const BOARD_TABS = [
   },
   {
     name: '자유게시판',
-    accessibleRoles: ['admin', 'active', 'associate', 'guest'],
+    accessibleRoles: ['admin', 'active', 'graduate', 'associate', 'guest'],
     key: BOARD_TYPE.freedom,
     href: NEW_PATH.boardList.url({
       boardType: BOARD_TYPE.freedom,
