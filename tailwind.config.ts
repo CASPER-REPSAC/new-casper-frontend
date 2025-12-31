28725ca91a6fea4248720c88b925ad90de4fd8dc
Jeong Wonjun <jwj1219j@naver.com>
2025-12-31 11:36:51 +0900

    fix: darktheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ import { fontFamily } from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-  darkMode: ['class', "html[data-theme='dark']"],
+  darkMode: 'class',
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
