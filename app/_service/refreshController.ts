2406970ca98e66b61ed921e32de96e6be31c7de4
jijiseong <jiseong5036@gmail.com>
2026-02-05 22:56:27 +0900

    feat: refresh

---
+++ app/_service/refreshController.ts
@@ -0,0 +1,88 @@
+/**
+ * @description Refresh token queue manager
+ * - managing queue of 401 failed requests
+ * - managing refreshing state
+ * - retrying failed requests
+ */
+
+class RefreshTokenController<RequestResponse, RefreshResponse> {
+  public isRefreshing = false;
+  private refreshSubscribers: {
+    requestFn: () => Promise<RequestResponse>;
+    resolve: (value: RequestResponse) => void;
+    reject: (error: unknown) => void;
+  }[] = [];
+  private refreshPromise: Promise<RefreshResponse> | null;
+
+  constructor() {
+    this.isRefreshing = false;
+    this.refreshSubscribers = [];
+    this.refreshPromise = null;
+  }
+
+  public retryFailedRequests = async () => {
+    try {
+      await Promise.all(
+        this.refreshSubscribers.map(({ requestFn, resolve, reject }) =>
+          requestFn().then(resolve).catch(reject),
+        ),
+      );
+    } finally {
+      this.refreshSubscribers = [];
+    }
+  };
+
+  public addFailedRequest = (requestFn: () => Promise<RequestResponse>) => {
+    const promise = new Promise<RequestResponse>((resolve, reject) => {
+      this.refreshSubscribers.push({ requestFn, resolve, reject });
+      return requestFn;
+    });
+
+    return promise;
+  };
+
+  public executeRefreshOnce = async (
+    requestFn: () => Promise<RefreshResponse>,
+  ) => {
+    if (this.isRefreshing) return this.refreshPromise;
+
+    this.isRefreshing = true;
+
+    this.refreshPromise = (async () => {
+      try {
+        const res = await requestFn();
+        return res;
+      } finally {
+        this.isRefreshing = false;
+        this.refreshPromise = null;
+      }
+    })();
+
+    return this.refreshPromise;
+  };
+
+  public get _isRefreshing_onlyForTest(): boolean {
+    return this.isRefreshing;
+  }
+
+  public set _setIsRefreshing_onlyForTest(value: boolean) {
+    this.isRefreshing = value;
+  }
+
+  public get _refreshSubscribers_onlyForTest() {
+    return this.refreshSubscribers;
+  }
+
+  public set _setRefreshSubscribers_onlyForTest(
+    value: {
+      requestFn: () => Promise<RequestResponse>;
+      resolve: (value: unknown) => void;
+      reject: (error: unknown) => void;
+    }[],
+  ) {
+    this.refreshSubscribers = value;
+  }
+}
+
+const refreshTokenController = new RefreshTokenController();
+export default refreshTokenController;
